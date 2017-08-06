1f656cb746e59ea2082d1df84bf298da6486f12e
justyna <j.pokrzywinska@gmail.com>
2017-08-06 17:42:33 +0200

    added new mapping - fibonacci

--- src/main/java/com/justyna/demo/DemoController.java
+++ src/main/java/com/justyna/demo/DemoController.java
@@ -3,6 +3,7 @@ package com.justyna.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -14,12 +15,21 @@ public class DemoController {
 
     @Autowired
     private DataValidator dataValidator;
+    @Autowired
+    private FibonacciSequenceService fibonacciSequenceService;
+
 
     @GetMapping(value = "/hello")
     @ResponseBody
     public String getData(@RequestParam String name, @RequestParam String surname, @RequestParam int number) {
         dataValidator.checkName(name);
         return name + " " + surname + " " + number;
+    }
+
+    @GetMapping(value = "/fibonacci/{number}")
+    @ResponseBody
+    public String computeFibonacciNumber(@PathVariable("number") int number) {
+        return number + " element ciagu Fibonacciego to: " + fibonacciSequenceService.computeFibonacciNumberIterative(number);
     }
 
 
